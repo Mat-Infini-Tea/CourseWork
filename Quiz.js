cc1e26be271130e55a9756ff6f4c0bcdd3dff9db
KavehNejad <56351092+KavehNejad@users.noreply.github.com>
2019-11-15 12:52:56 +0000

    Update Quiz.js

--- Quiz.js
+++ Quiz.js
@@ -1,10 +1,12 @@
-var quizQnA = [
+//Kaveh Nejad 1905933
+//JS for the Quiz part of the Games page(Titled "quiz")
+var quizQnA = [//Question - Correct Answer - 3 incorrect answers
     ["What is his off-stage name?","Nicolas Kim Coppola","Nicolas Cageson","Nicolas Coppa","Nicolas Harry Cage"],
     ["In which year was he born","1964","1965","1966","1967"],
     ["In  which year did he start acting?","1981","1980","1983","1984"]
 ];
 
-var QuizButtonIDs = ["quizOne", "quizTwo", "quizThree", "quizFour"];
+var QuizButtonIDs = ["quizOne", "quizTwo", "quizThree", "quizFour"]; // html IDs
 
 var quizPreviousQnA = [];
 var quizScore = 0;
@@ -15,7 +17,7 @@ var quizButtonIDsClone;
 var quizCorrectAnswser = "";
 var quizLastCorrect = "";
 
-function shuffle(arr){
+function shuffle(arr){ // shuffles a array. meant to shuffle the butttons so there in random order
     newArr = [];
     arrayLength = arr.length;
     for(let i = 0; i < arrayLength; i++){
@@ -24,7 +26,7 @@ function shuffle(arr){
     return newArr;
 }
 
-function dublicate(arr){
+function dublicate(arr){ // dublicates an Array
     var newArr = [];
     arrLength = arr.length;
     for (let i = 0; i < arrLength; i++){
@@ -34,11 +36,11 @@ function dublicate(arr){
 }
 
 
-function quizUpdateTextBoxValues(){
-    if(quizAttemptCount != 0){
+function quizUpdateTextBoxValues(){ // updates all the values in the textboxes
+    if(quizAttemptCount != 0){// not the first time
         document.getElementById("quizlastCorrect").innerHTML = "Last correct : " + quizLastCorrect; // update lastcoorect text box
     }
-    quizLastCorrect = quizCorrectAnswser;
+    quizLastCorrect = quizCorrectAnswser; // for when we want to show which answer was correct
 
     document.getElementById("quizAttemptDisplay").innerHTML = "attempt : " + ++quizAttemptCount; 
     document.getElementById("quizScore").innerHTML = "Score : " + quizScore;
@@ -48,14 +50,14 @@ function quizUpdateTextBoxValues(){
     }
 }
 
-function quizUpdateButtons(){
-    quizButtonIDsClone = dublicate(QuizButtonIDs);
+function quizUpdateButtons(){// updates the buttons
+    quizButtonIDsClone = dublicate(QuizButtonIDs); // so we dont change the orrigional array
     quizCorrectButton = Math.ceil(Math.random() * QuizButtonIDs.length - 1); // assigns random button
     document.getElementById(quizButtonIDsClone.splice(quizCorrectButton ,1)).value = quizCorrectAnswser; // sets value of correct button and removes it from the array
     quizQnAClone = shuffle(quizQnAClone); //shuffle QnA
     quizButtonIDsClone = shuffle(quizButtonIDsClone); //shuffle Buttoons
     do{
-        document.getElementById(quizButtonIDsClone.shift()).value = quizQnAClone.shift();
+        document.getElementById(quizButtonIDsClone.shift()).value = quizQnAClone.shift(); // assigns the false answers to the buttons
     }while (quizButtonIDsClone.length > 0)
 }
 
@@ -68,14 +70,14 @@ function quizGetQnA(){
     quizCorrectAnswser = quizQnAClone.shift();
 }
 
-function quizMain() {
+function quizMain() { // the main function
     quizGetQnA();
     quizUpdateButtons();
     quizUpdateTextBoxValues();
 
 }
 
-function quizButtonQuessCheck(quizInputNum){
-    quizCorrectButton === quizInputNum? quizScore++ : quizScore--;
+function quizButtonQuessCheck(quizInputNum){ // html answer buttons call this function
+    quizCorrectButton === quizInputNum? quizScore++ : quizScore--; // checks your anwser and assigns a score(yes you can go below 0 on perpuse)
     quizMain();
 }
